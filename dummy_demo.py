c3f1390ecf042e432658a11142c45c959b8140f4
nickiestikkie <nickstikkel@gmail.com>
2023-09-20 12:25:48 +0200

    made initial population

--- dummy_demo.py
+++ dummy_demo.py
@@ -8,6 +8,15 @@
 import sys, os
 
 from evoman.environment import Environment
+from demo_controller import player_controller
+
+# imports other libs
+import time
+import numpy as np
+from math import sqrt
+import random
+
+
 
 experiment_name = 'dummy_demo'
 if not os.path.exists(experiment_name):
@@ -25,17 +34,48 @@ if headless:
     os.environ["SDL_VIDEODRIVER"] = "dummy"
 
 
+def init_population(iIndividuals, iN_vars, iL_bound, iU_bound):
+    population = np.random.uniform(iL_bound, iU_bound, (iIndividuals, iN_vars))
+    return population
+
+def init_simulation(iNum_of_neurons):
+    env = Environment(experiment_name=experiment_name,
+                  enemies=[8],
+                  playermode="ai",
+                  player_controller=player_controller(iNum_of_neurons),
+                  enemymode="static",
+                  level=2,
+                  speed="fastest",
+                  visuals=False)
+    return env
+
+
 def main():
     #magic numbers
-    number_of
+    iNum_of_individuals = 100
+    iNum_of_neurons = 10
+    iL_bound = -1
+    iU_bound = 1
+    iN_generations = 10
+    dStop_time = 3000
+
     
-    for generation in generations:
+    env = init_simulation(iNum_of_neurons)
+    iNum_of_vars = (env.get_num_sensors()+1)*iNum_of_neurons + (iNum_of_neurons+1)*5 #is this general?
+    population = init_population(iNum_of_individuals, iNum_of_vars, iL_bound, iU_bound)
+
+    
+    for i in range(iN_generations):
+        # Start timer (time in seconds)
+        timer = time.time() 
+
+
         fitness= evaluate(population)
         parents = selection #[[]]
         Variation
         population = survivor_selection
-        if stop_cond
+        if timer >= 100:
             break
-
+        if 
 if __name__ == "__main__":
     main()
